774fcb8436340f7d4948caa17857796d866946ef
Paul Hancock <phancock@williamhill.co.uk>
2019-10-28 15:05:34 +0000

    adding in dates variable

--- json.py
+++ json.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import math
+import datetime
 
 
 r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -13,4 +15,17 @@ all_prices = bitcoin_price_eur, bitcoin_price_gbp, bitcoin_price_usd
 
 #packages_str = json.dumps(package_json, indent=2)
 
-print (all_prices)
+bitcoin_today_usd = bitcoin_price_usd, datetime.datetime.now()
+bitcoin_today_gbp = bitcoin_price_gbp, datetime.datetime.now()
+bitcoin_today_eur = bitcoin_price_eur, datetime.datetime.now()
+
+bitcoin_today = bitcoin_today_usd, bitcoin_today_gbp, bitcoin_today_eur
+
+for x in bitcoin_today:
+    print (x)
+#print(bitcoin_today_usd, bitcoin_price_eur, bitcoin_price_gbp)
+#def bitcoin_price_test():
+
+
+#for x in all_prices:
+#    print (x)
