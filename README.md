dff31b9db5e7a4e7c73b4bef8871487437169293
Paul Hancock <phancock@williamhill.co.uk>
2019-10-29 09:40:37 +0000

    added commented code and also now able to get the price of bitcoin yesterday

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bitcoinPrices
 
-Creating some python to output the current value of bitcoin across GBP, EUR and USD 
+The aim of this code is to take the price of bitcoin today and compare it with yesterdays prices.
+
+The code uses the bitcoin api to work through the prices of bitcoin.
+
+This will then be packaged and deployed to AWS as a lambda function which will then alert if the goes up or down.
--- json.py
+++ json.py
@@ -1,28 +1,44 @@
+#Modules required to perform functions
 import requests
 import json
 import math
 import datetime
 
-
+#Using the bitcoin API to get the value of bitcoins today and yesterday
 r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 package_json = r.json()
 
+#Creating date variables for my comparisons function
+today = datetime.date.today()
+yesterday = today - datetime.timedelta(days = 1)
+
+#Parsing date from the JSON so that i only recieve the rate from dictionary
 bitcoin_price_usd = package_json['bpi']['USD']['rate']
 bitcoin_price_gbp = package_json['bpi']['GBP']['rate']
 bitcoin_price_eur = package_json['bpi']['EUR']['rate']
 
 all_prices = bitcoin_price_eur, bitcoin_price_gbp, bitcoin_price_usd
 
-#packages_str = json.dumps(package_json, indent=2)
-
-bitcoin_today_usd = bitcoin_price_usd, datetime.datetime.now()
-bitcoin_today_gbp = bitcoin_price_gbp, datetime.datetime.now()
-bitcoin_today_eur = bitcoin_price_eur, datetime.datetime.now()
+#Getting the rate of bitcoin today
+bitcoin_today_usd = bitcoin_price_usd, today
+bitcoin_today_gbp = bitcoin_price_gbp, today
+bitcoin_today_eur = bitcoin_price_eur, today
 
 bitcoin_today = bitcoin_today_usd, bitcoin_today_gbp, bitcoin_today_eur
 
-for x in bitcoin_today:
+#Getting the rate of bitcoin yesterday
+bitcoin_yesterday_usd = bitcoin_price_usd, yesterday
+bitcoin_yesterday_gbp = bitcoin_price_gbp, yesterday
+bitcoin_yesterday_eur = bitcoin_price_eur, yesterday
+
+bitcoin_yesterday = bitcoin_yesterday_usd, bitcoin_yesterday_gbp, bitcoin_yesterday_eur
+
+#printing out variable to check code runs as expected.
+for x in bitcoin_yesterday:
     print (x)
+
+
+#packages_str = json.dumps(package_json, indent=2)
 #print(bitcoin_today_usd, bitcoin_price_eur, bitcoin_price_gbp)
 #def bitcoin_price_test():
 
